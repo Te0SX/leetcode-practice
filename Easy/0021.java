309533fe6ab8df4232155f5c70cf0064bfc7610d
te0sx <te0sx@yahoo.gr>
2021-06-15 21:44:55 +0100

    Create 0021

---
+++ Easy/0021.java
@@ -0,0 +1,26 @@
+// 0021. Merge Two Sorted Lists
+
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(0);
+        ListNode handler = head;
+        while(l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                handler.next = l1;
+                l1 = l1.next;
+            } else {
+                handler.next = l2;
+                l2 = l2.next;
+            }
+            handler = handler.next;
+        }
+        
+        if (l1 != null) {
+            handler.next = l1;
+        } else if (l2 != null) {
+            handler.next = l2;
+        }
+        
+        return head.next;
+    }
+}
