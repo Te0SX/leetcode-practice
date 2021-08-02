f939d6618879ac70fcd5f7420077e95ccdbd99b0
Theodoros Vrakas <te0sx@yahoo.gr>
2021-08-02 22:10:16 +0100

    Complete 0283

--- Easy/0283.java
+++ Easy/0283.java
@@ -2,15 +2,15 @@
 
 class Solution {
     public void moveZeroes(int[] nums) {
-    if(nums == null || nums.length == 0) return;
-    int k = 0;
-    for(int i=0; i < nums.length; i++) {
-        if(nums[i] != 0) {
-            nums[k++] = nums[i];
+        if(nums == null || nums.length == 0) return;
+        int k = 0;
+        for(int i=0; i < nums.length; i++) {
+            if(nums[i] != 0) {
+                nums[k++] = nums[i];
+            }
         }
-    }
-    for(int n=1; n <= k; n++) {
-        nums[nums.length-1-n] = 0;
-    }
+        for(int n=1; n < k; n++) {
+            nums[nums.length-n] = 0;
+        }
     }
 }
