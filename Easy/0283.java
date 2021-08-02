620b4a7a3c75bf3884caab28abaec79689131832
Theodoros Vrakas <te0sx@yahoo.gr>
2021-08-02 22:56:56 +0100

    Complete 0283

--- Easy/0283.java
+++ Easy/0283.java
@@ -5,12 +5,10 @@ class Solution {
         if(nums == null || nums.length == 0) return;
         int k = 0;
         for(int i=0; i < nums.length; i++) {
-            if(nums[i] != 0) {
-                nums[k++] = nums[i];
-            }
+            if(nums[i] != 0) nums[k++] = nums[i];
         }
-        for(int n=1; n < k; n++) {
-            nums[nums.length-n] = 0;
+        while(k < nums.length) {
+            nums[k++] = 0;
         }
     }
 }
