92ef26a108311cef41bb5660bcaab1b71f2f69ff
Theodoros Vrakas <te0sx@yahoo.gr>
2021-08-02 21:58:01 +0100

    Complete 0283

---
+++ Easy/0283.java
@@ -0,0 +1,16 @@
+// 0283 - Move Zeroes - https://leetcode.com/problems/move-zeroes/
+
+class Solution {
+    public void moveZeroes(int[] nums) {
+    if(nums == null || nums.length == 0) return;
+    int k = 0;
+    for(int i=0; i < nums.length; i++) {
+        if(nums[i] != 0) {
+            nums[k++] = nums[i];
+        }
+    }
+    for(int n=1; n <= k; n++) {
+        nums[nums.length-1-n] = 0;
+    }
+    }
+}
