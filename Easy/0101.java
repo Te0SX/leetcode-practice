8b1d398b7d75e9ab8b2c05d667ab3974808154be
Theodoros Vrakas <te0sx@yahoo.gr>
2021-08-29 21:53:11 +0100

    Complete 101

---
+++ Easy/0101.java
@@ -0,0 +1,37 @@
+// Symmetric Tree
+
+// 25.42% faster, 30.14% less mem
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) return true;
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root.left);
+        stack.push(root.right);
+        while (!stack.empty()) {
+            TreeNode n1 = stack.pop(), n2 = stack.pop();
+            if (n1 == null && n2 == null) continue;
+            if (n1 == null || n2 == null || n1.val != n2.val) return false;
+            stack.push(n1.left);
+            stack.push(n2.right);
+            stack.push(n1.right);
+            stack.push(n2.left);
+        }
+        return true;
+    }
+}
+
+// 100% faster, less than 8.77%
+// // Recurisve
+// class Solution {
+//     public boolean isSymmetric(TreeNode root) {
+//         return root==null || isSymmetricHelp(root.left, root.right);
+//     }
+
+//     private boolean isSymmetricHelp(TreeNode left, TreeNode right){
+//         if(left==null || right==null)
+//             return left==right;
+//         if(left.val!=right.val)
+//             return false;
+//         return isSymmetricHelp(left.left, right.right) && isSymmetricHelp(left.right, right.left);
+//     }
+// }
