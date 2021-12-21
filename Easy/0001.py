12a1deaafb717dc56ee2cfe6110605843f06dff5
tdvr <te0sx@yahoo.gr>
2021-12-21 23:55:08 +0200

    Python? Here we go again...

---
+++ Easy/0001.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        dict = {}
+        for i,n in enumerate(nums):
+            m = target - n
+            if m in d:
+                return [d[m],i]
+            else:
+                d[n] = i
+
+    
